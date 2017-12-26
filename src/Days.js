69930e5091b019a7b98469b83afce3746c3d9db4
Vitaly Savchuk <vitalysavchuk2312@gmail.com>
2017-12-26 13:48:11 +0200

    before changing

--- src/Days.js
+++ src/Days.js
@@ -6,6 +6,13 @@ export default class Days extends Component {
     let days = this.props.days(this.props.year, this.props.month, 1);
     days = days.map((item, index) => {
       let arr = item.map((item2,index2) => {
+        if(index == 0 && item2 - 1 > 20) {
+          return(
+            <div key={index2} className="calendar-container__body-month-days-item">
+              <span className="calendar-container__body-month-days-item-number grey">{item2}</span>
+            </div>
+          );
+        }
         return(
           <div key={index2} className="calendar-container__body-month-days-item">
             <span className="calendar-container__body-month-days-item-number">{item2}</span>
